2c091345da00028c9ce2e7723ea9911976d288a0
Ahsaan Ullah <ahsaanu70@gmail.com>
2025-01-26 18:49:34 +0500

    Singup page frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { BrowserRouter , Routes , Route } from 'react-router-dom'
+import { LoginPage , SignupPage } from './Routes'
 
 const App = () => {
   return (
-    <div className='text-red-500'>
-      Hello World
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/login" element = {<LoginPage/>}/>
+        <Route path = "/signup" element = {<SignupPage/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ Frontend/src/Routes.js
@@ -0,0 +1,4 @@
+import LoginPage from './Pages/LoginPage.jsx';
+import SignupPage from './Pages/SignupPage.jsx';
+
+export { LoginPage, SignupPage };
